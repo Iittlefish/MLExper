06c99543cac4b99a33f78cc54d0bd9a12a162ce2
unknown <st1030283215852@gmail.com>
2025-03-05 21:29:18 +0800

    style: add annotation feat: compelte model fit and difference the main.py between main.ipynb.
    style, add annotation of dataPreprocess.
    feat, the feature of main.py is trun the WESAD filfe to .csv file.
    and the feature of main.ipynb is traning and geting the accurancy score of modle.

--- dataPreprocess.py
+++ dataPreprocess.py
@@ -6,8 +6,7 @@ class DataProcesser:
     __filePath = []
     __data = None   
 
-    def __init__(self):
-        self.__data = self.__readFile()
+    def __init__(self):     #init new object. get all the file path of dataset
         self.__file = []
         self.__bassPath =""
         
@@ -22,7 +21,7 @@ class DataProcesser:
             filePath.append(str(tempPath))
             self.__filePath.append(str(filePath[i]))
 
-    def __readFile(self):
+    def __readFile(self):       #read WESAD file, get the data and concat to a dataframe
         label = [] 
         acc = []
         ecg = []
@@ -38,21 +37,23 @@ class DataProcesser:
         for i in range(len(self.__filePath)):
             with open(self.__filePath[i], "rb") as f:
                 temp = pickle.load(f, encoding="bytes")
-                chest = temp[b"signal"][b"chest"]
+                signalData = temp[b"signal"]
+                chest = signalData[b"chest"]
                 tempList  = chest[b"ACC"].tolist()
                 tempArry = np.array(tempList).T
+                length = len(tempArry[0])
             tempData ={
-                "ACC": chest[b'ACC'].tolist(),
                 "ACC0": tempArry[0],
                 "Acc1": tempArry[1],
                 "ACC2": tempArry[2],
-                "ECG": chest[b'ECG'].tolist(),
-                "EMG": chest[b'EMG'].tolist(),
-                "EDA": chest[b'EDA'].tolist(),
-                "resp": chest[b'Resp'].tolist(),
-                "temp": chest[b'Temp'].tolist(),
-                "label": temp[b"label"].tolist(),
+                "ECG": chest[b'ECG'].reshape(length,),
+                "EMG": chest[b'EMG'].reshape(length,),
+                "EDA": chest[b'EDA'].reshape(length,),
+                "resp": chest[b'Resp'].reshape(length,),
+                "temp": chest[b'Temp'].reshape(length,),
+                "label": temp[b"label"].reshape(length,),
             }
+            print(tempData)
             tempData = pd.DataFrame.from_dict(tempData)
             randomData = self.getRandomData(tempData)
             print(len(randomData))
@@ -63,24 +64,19 @@ class DataProcesser:
         print(len(self.__data))
         return  self.__data
     
-    def getData(self):
+    def getData(self):      #return dataset
         data = self.__readFile()
         self.__data = data
         return self.__data
-
-
-    def getAllDataFrame(self):
-        df = self.__readFile() 
-        return df
     
-    def __GroupByLabel(self, data):
+    def __GroupByLabel(self, data):     #group the dataset by label
         df = data
         label = df.groupby("label")
         one = label.get_group(1)
         two = label.get_group(2)
         return[one, two]
     
-    def getRandomData(self, data ):
+    def getRandomData(self, data ):     #return sample and randon data
         groupData = self.__GroupByLabel(data)
         dataOne = groupData[0].sample(n=40, random_state= 3)
         dataTwo = groupData[1].sample(n=40, random_state= 3)
@@ -89,12 +85,12 @@ class DataProcesser:
         print(len(randomData))
         return randomData
     
-    def toCsv(self):
+    def toCsv(self):        #turn the WESAD file to the .CSV file, WESAD file is too big.
         if (type(self.__data) != pd.DataFrame):
             self.__readFile()
         self.__data.to_csv("./dataSet.csv")
     
-    def toNpArray(self):
+    def toNpArray(self):    #turn the dataset to np array by read WESAD file.
         if (self.__data == None):
             self.__readFile()
         npArray = self.__data.to_numpy()
--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 from dataPreprocess import DataProcesser
-from sklearn.model_selection import train_test_split
-from pandas import DataFrame
 
-'''
+
+# get the dataset from WESAD and turn into .csv file
 dp = DataProcesser()
 data = dp.getData()
-data = data.to_numpy()
-print(data)
+dp.toCsv()
 
-label = data["label"]
+
+
+
+#data = pd.read_csv("./dataSet.csv")
+'''
 data = data.drop("label", axis= 1)
 xTrain, xTest, yTrain, yTest = train_test_split(data, label, test_size=0.2, random_state=302)
+
+model = AdaBoostClassifier(random_state=304, learning_rate= 0.8)
+model.fit(xTrain, yTrain)
+model.predict(xTest, yTest)
 '''
-dP = DataProcesser()
-data = dP.getData()
-dP.toCsv()
